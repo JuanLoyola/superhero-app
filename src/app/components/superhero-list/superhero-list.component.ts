cb16150662109ef00a04d158f8f4215b720ebe11
JuanL <juanjoloyola@hotmail.com>
2024-11-14 09:56:47 -0300

    format using prettier

--- src/app/components/superhero-list/superhero-list.component.ts
+++ src/app/components/superhero-list/superhero-list.component.ts
@@ -13,93 +13,94 @@ import { ModalAdd } from '../modal-add/modal-add.component';
 import { ModalEdit } from '../modal-add copy/modal-edit.component';
 
 @Component({
-  selector: 'app-superhero-list',
-  templateUrl: './superhero-list.component.html',
-  styleUrls: ['./superhero-list.component.css'],
-  standalone: true,
-  imports: [
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTableModule,
-    MatButtonModule,
-    MatPaginatorModule,
-    FormsModule,
-    MatIconModule,
-    ModalAdd,
-    ModalEdit,
-  ],
+	selector: 'app-superhero-list',
+	templateUrl: './superhero-list.component.html',
+	styleUrls: ['./superhero-list.component.css'],
+	standalone: true,
+	imports: [
+		MatCardModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatTableModule,
+		MatButtonModule,
+		MatPaginatorModule,
+		FormsModule,
+		MatIconModule,
+		ModalAdd,
+		ModalEdit,
+	],
 })
 export class SuperheroListComponent implements OnInit {
-  superheroes: Superhero[] = [];
-  filteredSuperheroes: Superhero[] = [];
-  searchTerm: string = '';
-  currentPage: number = 1;
-  itemsPerPage: number = 5;
+	superheroes: Superhero[] = [];
+	filteredSuperheroes: Superhero[] = [];
+	searchTerm: string = '';
+	currentPage: number = 1;
+	itemsPerPage: number = 5;
 
-  openModalAdd = false;
-  openModalEdit = false;
+	openModalAdd = false;
+	openModalEdit = false;
+	showConfirm = false;
 
-  selectedHero!: Superhero;
+	selectedHero!: Superhero;
 
-  private superheroService = inject(SuperheroService);
+	private superheroService = inject(SuperheroService);
 
-  ngOnInit(): void {
-    this.getAll();
-  }
+	ngOnInit(): void {
+		this.getAll();
+	}
 
-  getAll() {
-    this.superheroService.getAllSuperheroes().subscribe((heroes) => {
-      this.superheroes = heroes;
+	getAll() {
+		this.superheroService.getAllSuperheroes().subscribe((heroes) => {
+			this.superheroes = heroes;
 
-      this.filteredSuperheroes = heroes;
-    });
-  }
+			this.filteredSuperheroes = heroes;
+		});
+	}
 
-  handlePageChange(page: number): void {
-    this.currentPage = page;
-  }
+	handlePageChange(page: number): void {
+		this.currentPage = page;
+	}
 
-  handleDelete(superhero: Superhero): void {
-    this.superheroService.deleteSuperhero(superhero.id);
-  }
+	handleDelete(superhero: Superhero): void {
+		this.superheroService.deleteSuperhero(superhero.id);
+	}
 
-  filterSuperheroes(): void {
-    const searchTermLower = this.searchTerm.toLowerCase();
+	filterSuperheroes(): void {
+		const searchTermLower = this.searchTerm.toLowerCase();
 
-    this.filteredSuperheroes = this.superheroes.filter((hero) => {
-      return ['name', 'description', 'id'].some((field) =>
-        (hero as any)[field]?.toString().toLowerCase().includes(searchTermLower)
-      );
-    });
-  }
+		this.filteredSuperheroes = this.superheroes.filter((hero) => {
+			return ['name', 'description', 'id'].some((field) =>
+				(hero as any)[field]?.toString().toLowerCase().includes(searchTermLower)
+			);
+		});
+	}
 
-  handleAdd() {
-    this.openModalAdd = !this.openModalAdd;
-  }
+	handleAdd() {
+		this.openModalAdd = !this.openModalAdd;
+	}
 
-  handleEdit(superhero: Superhero): void {
-    console.log(superhero);
-    this.openModalEdit = true;
-    this.selectedHero = superhero;
-  }
+	handleEdit(superhero: Superhero): void {
+		console.log(superhero);
+		this.openModalEdit = true;
+		this.selectedHero = superhero;
+	}
 
-  onHeroAdded(newHero: Superhero): void {
-    // TODO: esto deberia pushear a un array que tengamos en sessionStorage y luego traer esos items junto al nuevo
-    this.superheroes.push(newHero);
-    this.filteredSuperheroes = [...this.superheroes];
+	onHeroAdded(newHero: Superhero): void {
+		// TODO: esto deberia pushear a un array que tengamos en sessionStorage y luego traer esos items junto al nuevo
+		this.superheroes.push(newHero);
+		this.filteredSuperheroes = [...this.superheroes];
 
-    this.openModalAdd = false;
-  }
+		this.openModalAdd = false;
+	}
 
-  onHeroEdited(editedHero: Superhero): void {
-    const index = this.superheroes.findIndex(
-      (hero) => hero.id === editedHero.id
-    );
-    if (index !== -1) {
-      this.superheroes[index] = editedHero;
-      this.filteredSuperheroes = [...this.superheroes];
-    }
-    this.openModalEdit = false;
-  }
+	onHeroEdited(editedHero: Superhero): void {
+		const index = this.superheroes.findIndex(
+			(hero) => hero.id === editedHero.id
+		);
+		if (index !== -1) {
+			this.superheroes[index] = editedHero;
+			this.filteredSuperheroes = [...this.superheroes];
+		}
+		this.openModalEdit = false;
+	}
 }
--- src/app/services/superhero.service.ts
+++ src/app/services/superhero.service.ts
@@ -4,58 +4,58 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class SuperheroService {
 
-  constructor() { }
+	constructor() { }
 
-  private superheroes: Superhero[] = [
-    { id: 1, name: 'Solar Flare', description: 'A former astronaut who gained the power to manipulate solar energy after a space mission accident' },
-    { id: 2, name: 'Shadow Walker', description: 'A mysterious vigilante with the ability to melt into shadows and travel through darkness' },
-    { id: 3, name: 'Terra Force', description: 'An environmentalist who can control earth elements and communicate with plants' },
-    { id: 4, name: 'Quantum Mind', description: 'A brilliant scientist with telepathic abilities and superhuman intelligence' },
-    { id: 5, name: 'Crystal Guardian', description: 'A warrior with the power to create and manipulate crystal structures for both defense and offense' },
-    { id: 6, name: 'Spiderman', description: 'A young man who gained spider-like abilities after being bitten by a radioactive spider, allowing him to climb walls and shoot webs' },
-    { id: 7, name: 'Superman', description: 'The last son of Krypton, with super strength, flight ability, and other superhuman powers' },
-    { id: 8, name: 'Pepsiman', description: 'The corporate mascot turned hero, with a mission to deliver Pepsi and refresh the world' },
-  ]
+	private superheroes: Superhero[] = [
+		{ id: 1, name: 'Solar Flare', description: 'A former astronaut who gained the power to manipulate solar energy after a space mission accident' },
+		{ id: 2, name: 'Shadow Walker', description: 'A mysterious vigilante with the ability to melt into shadows and travel through darkness' },
+		{ id: 3, name: 'Terra Force', description: 'An environmentalist who can control earth elements and communicate with plants' },
+		{ id: 4, name: 'Quantum Mind', description: 'A brilliant scientist with telepathic abilities and superhuman intelligence' },
+		{ id: 5, name: 'Crystal Guardian', description: 'A warrior with the power to create and manipulate crystal structures for both defense and offense' },
+		{ id: 6, name: 'Spiderman', description: 'A young man who gained spider-like abilities after being bitten by a radioactive spider, allowing him to climb walls and shoot webs' },
+		{ id: 7, name: 'Superman', description: 'The last son of Krypton, with super strength, flight ability, and other superhuman powers' },
+		{ id: 8, name: 'Pepsiman', description: 'The corporate mascot turned hero, with a mission to deliver Pepsi and refresh the world' },
+	]
 
-  private superheroes$ = new BehaviorSubject<Superhero[]>(this.superheroes)
+	private superheroes$ = new BehaviorSubject<Superhero[]>(this.superheroes)
 
 
-  getAllSuperheroes(): Observable<Superhero[]> {
-    return this.superheroes$.asObservable()
-  }
+	getAllSuperheroes(): Observable<Superhero[]> {
+		return this.superheroes$.asObservable()
+	}
 
-  getSuperheroById(id: number): Observable<Superhero | undefined> {
-    return this.superheroes$.pipe(
-      map(heroes => heroes.find((hero: Superhero) => hero.id === id))
-    );
-  }
+	getSuperheroById(id: number): Observable<Superhero | undefined> {
+		return this.superheroes$.pipe(
+			map(heroes => heroes.find((hero: Superhero) => hero.id === id))
+		);
+	}
 
-  getSuperheroesByName(name: string): Observable<Superhero[]> {
-    return this.superheroes$.pipe(
-      map(heroes => heroes.filter((hero: Superhero) => hero.name.toLowerCase().includes(name.toLowerCase())))
-    );
-  }
+	getSuperheroesByName(name: string): Observable<Superhero[]> {
+		return this.superheroes$.pipe(
+			map(heroes => heroes.filter((hero: Superhero) => hero.name.toLowerCase().includes(name.toLowerCase())))
+		);
+	}
 
-  addSuperhero(superhero: Superhero): void {
-    this.superheroes.push(superhero)
-    this.superheroes$.next(this.superheroes)
-  }
+	addSuperhero(superhero: Superhero): void {
+		this.superheroes.push(superhero)
+		this.superheroes$.next(this.superheroes)
+	}
 
-  editSuperhero(superhero: Superhero): void {
-    const index = this.superheroes.findIndex(hero => hero.id === superhero.id)
+	editSuperhero(superhero: Superhero): void {
+		const index = this.superheroes.findIndex(hero => hero.id === superhero.id)
 
-    if (index !== -1) {
-      this.superheroes[index] = superhero
-      this.superheroes$.next(this.superheroes)
-    }
-  }
+		if (index !== -1) {
+			this.superheroes[index] = superhero
+			this.superheroes$.next(this.superheroes)
+		}
+	}
 
-  deleteSuperhero(id: number): void {
-    this.superheroes = this.superheroes.filter(hero => hero.id !== id)
-    this.superheroes$.next(this.superheroes)
-  }
+	deleteSuperhero(id: number): void {
+		this.superheroes = this.superheroes.filter(hero => hero.id !== id)
+		this.superheroes$.next(this.superheroes)
+	}
 }
